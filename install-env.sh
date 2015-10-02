106080c14cfbdf38e5dcf23d94bed6cf856878ee
csiron <csiron@hawk.iit.edu>
2015-10-02 01:08:50 -0500

    changed the command from launching instances to installing apache and git

--- install-env.sh
+++ install-env.sh
@@ -1,4 +1,4 @@
 #!./bin/bash
-aws ec2 run-instances --image-id ami-d05e75b8 --count 2 --instance-type t2.micro --key-name ITMO-444-vmware --security-group-ids sg-c7c2fea0 --subnet-id subnet-0aa7a97d --associate-public-ip-address
+sudo apt-get update -y  && sudo apt-get apache2 -y
 echo >> /tmp/hello.txt
 fi
