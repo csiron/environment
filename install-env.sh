082ea055b45cfd297c509178cd976d8c7f04877e
Craig <craig@localhost.localdomain>
2015-10-02 00:51:41 -0500

    added the script to generate instances

---
+++ install-env.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+aws ec2 run-instances --image-id ami-d05e75b8 --count 2 --instance-type t2.micro --key-name ITMO-444-vmware --security-group-ids sg-c7c2fea0 --subnet-id subnet-0aa7a97d --associate-public-ip-address
+echo >> /tmp/hello.txt
+fi
