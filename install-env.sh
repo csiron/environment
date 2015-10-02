47b1d15c3944d053b8a3121435ab7db5c92b0dff
csiron <csiron@hawk.iit.edu>
2015-10-02 01:35:18 -0500

    corrected the syntax needed

--- install-env.sh
+++ install-env.sh
@@ -1,4 +1,5 @@
 #!./bin/bash
-sudo apt-get update -y  && sudo apt-get apache2 -y && sudo apt-get git
-echo >> /tmp/hello.txt
+sudo apt-get update -y 
+sudo apt-get install apache2 -y 
+sudo apt-get install git -y
 fi
