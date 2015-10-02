0e01120582c7f1792d83705c64c7de8e6cb46399
csiron <csiron@hawk.iit.edu>
2015-10-02 17:08:47 -0500

    corrected the mv lines and removed fi

--- install-env.sh
+++ install-env.sh
@@ -4,12 +4,10 @@ sudo apt-get install -y apache2 git
 
 git clone https://github.com/csiron/Application.git
 
-mv ./Application/images ./var/www/html/images
-mv ./Application/page1.html ./var/www/html
-mv ./Application/page2.html ./var/www/html
+mv Application/images var/www/html/images
+mv Application/page1.html var/www/html
+mv Application/page2.html var/www/html
 
 echo > "Hello" /var/hello.txt
-
-fi
 
 
