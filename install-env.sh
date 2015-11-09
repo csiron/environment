e13debbc33d0ee94f52bceef82caf99e30b6835f
csiron <csiron@hawk.iit.edu>
2015-11-09 03:11:41 -0600

    added mv command for the setup.php file and changed the privileges for the vendor file

--- install-env.sh
+++ install-env.sh
@@ -10,7 +10,8 @@ sudo mv Application/images /var/www/html/images
 sudo mv Application/page1.html /var/www/html
 sudo mv Application/page2.html /var/www/html
 sudo mv Application/dbcreate.php /var/www/html
-chmod 755 Application/vendor
+sudo mv Application/setup.php /var/www/html
+chmod 600 Application/vendor
 sudo mv Application/vendor /var/www/html/vendor
 cd /var/www/html
 sudo rm index.html
