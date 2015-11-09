600f402c7640aa8af2d5412a9f2bba32ee89cfc5
csiron <csiron@hawk.iit.edu>
2015-11-09 03:16:33 -0600

    added the move command for the gallery file

--- install-env.sh
+++ install-env.sh
@@ -11,6 +11,7 @@ sudo mv Application/page1.html /var/www/html
 sudo mv Application/page2.html /var/www/html
 sudo mv Application/dbcreate.php /var/www/html
 sudo mv Application/setup.php /var/www/html
+sudo mv Application/gallery.php /var/www/html
 chmod 600 Application/vendor
 sudo mv Application/vendor /var/www/html/vendor
 cd /var/www/html
