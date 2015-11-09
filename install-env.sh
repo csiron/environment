ff566f5c5f8ae71c10b0dc5c33501082d386823f
csiron <csiron@hawk.iit.edu>
2015-11-09 06:55:31 -0600

    added php execute command

--- install-env.sh
+++ install-env.sh
@@ -14,9 +14,10 @@ sudo mv Application/setup.php /var/www/html
 sudo mv Application/gallery.php /var/www/html
 sudo mv Application/index.php /var/www/html
 sudo mv Application/submit.php /var/www/html
-chmod 600 Application/vendor
+chmod 755 Application/vendor
 sudo mv Application/vendor /var/www/html/vendor
 cd /var/www/html
+sudo php -r setup.php
 sudo rm index.html
 
 echo > "Hello" /var/hello.txt
