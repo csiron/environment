bffe3298f3dd0f72bee257b03f24f56cfcb61ad1
csiron <csiron@hawk.iit.edu>
2015-11-19 23:45:49 -0600

    updated and consolidated commands

--- install-env.sh
+++ install-env.sh
@@ -1,26 +1,18 @@
 #!/bin/bash
-sudo apt-get update -y 
+
+sudo apt-get update -y
 sudo apt-get install -y apache2 git php5 php5-curl mysql-client curl php5-mysql
-sudo curl -sS http://getcomposer.org/installer
-sudo php composer.phar require aws/aws-sdk-php
 
 git clone https://github.com/csiron/Application.git
 
-sudo mv Application/images /var/www/html/images
-sudo mv Application/page1.html /var/www/html
-sudo mv Application/page2.html /var/www/html
-sudo mv Application/dbcreate.php /var/www/html
-sudo mv Application/setup.php /var/www/html
-sudo mv Application/gallery.php /var/www/html
-sudo mv Application/index.php /var/www/html
-sudo mv Application/submit.php /var/www/html
-chmod 755 Application/vendor
-sudo mv Application/vendor /var/www/html/vendor
-cd /var/www/html
-sudo php -r setup.php
-chmod 600 setup.php
-sudo rm index.html
+sudo mv ./Application/images /var/www/html/images
+sudo mv ./Application/index.html /var/www/html
+sudo mv ./Application/*.php /var/www/html
 
-echo > "Hello" /var/hello.txt
+sudo curl -sS https://getcomposer.org/installer | sudo php &> /tmp/getcomposer.txt
 
+sudo php composer.phar require aws/aws-sdk-php &> /tmp/runcomposer.txt
 
+sudo mv vendor /var/www/html &> /tmp/movevendor.txt
+
+sudo php /var/www/html/setup.php &> /tmp/database-setup.txt
